3cd34174991bb1a0822deeaea112dbadb850f6ba
Jiahao Li <lijh15328076@gmail.com>
2025-07-16 22:48:05 +0900

    Configuration for storages on S3

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,13 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+INSTALLED_APPS += ['storages']
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = 'AKIA3ENGOPU2Y3EFSDUR'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'findingnothing-homeworks-bucket'
+AWS_S3_REGION_NAME = 'us-east-1'
