a6d857a28af574aba74820d8187d0cf9fe28c6eb
Jiahao Li <lijh15328076@gmail.com>
2025-07-22 00:44:45 +0900

    Fixing logout function

--- config/urls.py
+++ config/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('home.urls')),
     path('', include('accounts.urls')),
     path('', include('submissions.urls')),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
